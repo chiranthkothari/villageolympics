d8b2df9634bf4cb109d262c6e55740fce1b6dc4c
chiranthkothari <chiranthkothari96@gmail.com>
2021-09-10 17:21:25 +0530

    Added sports page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Homepage from "./Pages/Homepage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Nav } from "./Components/Homepage/Navbar";
+import SportsPage from "./Components/Sports/Sports";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/sports" component={SportsPage}></Route>
+          <Route path="/" component={Homepage}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
